3a29c81a410fff1bfa1a59164d9b861a398ac566
Mylonly <root@mylonly.com>
2016-07-13 22:45:18 +0800

    modify

--- article/models.py
+++ article/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Article(models.Model):
@@ -9,6 +9,7 @@ class Article(models.Model):
     category = models.CharField(max_length=50,blank=True)
     date_time = models.DateTimeField(auto_now_add=True)
     content = models.TextField(blank=True,null=True)
+    author = models.ForeignKey(User,null=True)
 
     def __unicode__(self):
         return self.title
