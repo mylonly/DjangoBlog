735c3e406c7d166606c7c6681466752f82cd59d9
Mylonly <root@mylonly.com>
2016-07-18 21:46:01 +0800

    修改wsgi.py

--- DjangoBlog/wsgi.py
+++ DjangoBlog/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
+sys.path.append(settings.BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoBlog.settings")
 
 application = get_wsgi_application()
