95b6f8448d96c45b2cd869a5b4f80c8bd037c042
Mylonly <root@mylonly.com>
2016-07-18 23:10:11 +0800

    添加tag,category各种模型

--- article/admin.py
+++ article/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from article.models import Article
+from article.models import Article,Tag,Category
 
 admin.site.register(Article)
+admin.site.register(Tag)
+admin.site.register(Category)
--- article/models.py
+++ article/models.py
@@ -4,9 +4,28 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    alias = models.SlugField(max_length=20)
+    super_category = models.ForeignKey('self',null=True,blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    alias = models.SlugField(max_length=20)
+
+    def __unicode__(self):
+        return self.name 
+
+
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    category = models.CharField(max_length=50,blank=True)
+    category = models.ForeignKey(Category,null=True)
+    tags = models.ManyToManyField(Tag,through='TagRelation',through_fields=('article','tag'))
     date_time = models.DateTimeField(auto_now_add=True)
     content = models.TextField(blank=True,null=True)
     author = models.ForeignKey(User,null=True)
@@ -16,3 +35,11 @@ class Article(models.Model):
 
     class Meta():
         ordering = ['-date_time']
+
+
+class TagRelation(models.Model):
+    article = models.ForeignKey(Article)
+    tag = models.ForeignKey(Tag)
+
+    def __unicode__(self):
+        return tag.name+':'+article.name
