e27ce6884167a76991a24e00558699a5d74f4cb0
Mylonly <dim@mylonly.com>
2016-07-18 16:36:30 +0800

    modify

--- article/views.py
+++ article/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
 from django.http import Http404
+from django.template import RequestContext, loader
+
 # Create your views here.
 
 def home(request):
     post_list = Article.objects.all()
-    return render(request,'home.html',{'post_list':post_list})
+    return render(request,'article/home.html',{'post_list':post_list})
 
 def detail(request,id):
     try:
